ff6c13555e4d4093aa179d8c418a0e4cb2ecce10
itsironic <itsironik@gmail.com>
2021-04-21 18:33:30 +0900

    생활코딩 React: state 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Subject from './components/Subject';
+import Navigation from './components/Navigation';
+import Content from './components/Content';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:"WEB", desc:"World Wide Web"},
+      navigation:[
+        {id:1, title:"HTML", desc:"HTML is Hyper Text Markup Language."},
+        {id:2, title:"CSS", desc:"CSS is Cascading Style Sheets."},
+        {id:3, title:"JavaScript", desc:"JavaScript is programming language that conforms to the ECMAScript specification."}
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title} 
+          desc={this.state.subject.desc}/>
+        <Navigation 
+          data={this.state.navigation}/>
+        <Content title="HTML" desc="HTML is Hyper Text Markup Language."/>
+      </div>
+    );
+  }
 }
 
 export default App;
